b99551dfb19783bbd7537d16663026a2ce59835f
Marlovy <cuerorenteriamarlovy33@gmail.com>
2023-07-01 10:28:11 -0500

    carpeta-objetos

---
+++ 04-frontend/css/03-js/03-objetos/script.js
@@ -0,0 +1,16 @@
+var gato = {
+  nombre: "sweet",
+  color: "negro",
+  edad: "3",
+  hembra: true,
+  hermanos: ["oscar", "michii", "matias"],
+  maullar: function () {
+    return "miauuuuuuuuu";
+  },
+  ronronear: function () {
+    return "purrrrr";
+  },
+};
+gato.edad = 6;
+hembra.gato = false;
+console.log(gato);
