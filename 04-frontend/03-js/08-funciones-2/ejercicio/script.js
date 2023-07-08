89f7a170e2a03c28664c6706e40b1e9cb4adb5cb
Marlovy <cuerorenteriamarlovy33@gmail.com>
2023-07-08 12:45:23 -0500

    actualizacion

--- 04-frontend/03-js/08-funciones-2/ejercicio/script.js
+++ 04-frontend/03-js/08-funciones-2/ejercicio/script.js
@@ -180,7 +180,8 @@ function agregarSimboloExclamacion(str) {
   // Agrega un símbolo de exclamación al final de la string "str" y devuelve una nueva string
   // Ejemplo: "hello world" pasaría a ser "hello world!"
   // Tu código:
-}
+} 
+
 
 function combinarNombres(nombre, apellido) {
   // Devuelve "nombre" y "apellido" combinados en una string y separados por un espacio.
